8d36cd77ef0ab8ef86834462f39116d6448c6388
xternal7 <zp8324@student.uni-lj.si>
2018-05-07 21:58:11 +0200

    conditional stretching

--- js/modules/Stretcher.js
+++ js/modules/Stretcher.js
@@ -5,11 +5,51 @@
 
 
 class Stretcher {
-    // internal variables
+  // internal variables
 
 
-    // functions
-    constructor() {
+  // functions
+  constructor() {
 
+  }
+
+  static conditionalStretch(videoDimensions, maxDifferencePercent){
+    // samo razširjamo, nikoli krčimo
+    // only stretch, no shrink
+
+    var x, y;
+
+    x = videoDimensions.width / videoDimensions.actualWidth;
+    y = videoDimensions.height / videoDimensions.actualHeight;
+
+    var dmax = 1 + maxDifferencePercent;
+
+    if(x < 1 || x > dmax){
+      x = 1;
     }
+    if(y < 1 || y > dmax){
+      y = 1;
+    }
+
+    return {
+      x: x,
+      y: y
+    }
+  }
+
+  static calculateStretch(videoDimensions) {
+    // naj ponovim: samo razširjamo, nikoli krčimo
+    // let me reiterate: only stretch, no shrink
+
+    var stretch = {x: 1, y: 1};
+
+    if (videoDimensions.actualWidth < videoDimensions.width) {
+      stretch.x = videoDimensions.width / videoDimensions.actualWidth;
+    }
+    if (videoDimensions.actualHeight < videoDimensions.height){
+      stretch.y = videoDimensions.height / videoDimensions.actualHeight;
+    }
+
+    return stretch;
+  }
 }
