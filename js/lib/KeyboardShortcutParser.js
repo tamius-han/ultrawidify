460fa8b1de05b576c2bbc6a7705c9cd37e46fd40
Tamius Han <ziga.pirih@hotmail.com>
2018-12-01 01:35:22 +0100

    forgot to commit new files

---
+++ js/lib/KeyboardShortcutParser.js
@@ -0,0 +1,21 @@
+class KeyboardShortcutParser {
+  static parseShortcut(keypress) {
+    var shortcutCombo = '';
+  
+    if (keypress.ctrlKey) {
+      shortcutCombo += 'Ctrl + ';
+    }
+    if (keypress.shiftKey) {
+      shortcutCombo += 'Shift + ';
+    }
+    if (keypress.metaKey) {
+      shortcutCombo += 'Meta + ';
+    }
+    if (keypress.altKey) {
+      shortcutCombo += 'Alt + ';
+    }
+    shortcutCombo += keypress.key.toUpperCase();
+  
+    return shortcutCombo;
+  }
+}
