108297fe1b81733fe47b129a21abd2c9dc2f09a9
Tamius Han <tamius.han@gmail.com>
2019-04-12 21:06:01 +0200

    Section for editing keyboard shortcuts is reasonably complete.

---
+++ src/options/common/ConfirmationPopup.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="full-screen"
+       @click="cancel()"
+  >
+    <div class="dialog-box flex flex-column" @click="$event.stopPropagation()">
+      <div>
+        {{dialogText}}
+      </div>
+      <div class="flex flex-row flex-end">
+        <ShortcutButton class="flex b3 button"
+                        label="Ok"
+                        @click.native="confirm()"
+        />
+        <ShortcutButton class="flex b3 button"
+                        label="Cancel"
+                        @click.native="cancel()"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShortcutButton from '../../common/components/shortcut-button.vue' 
+
+export default {
+  props: {
+    dialogText: String,
+  },
+  components: {
+    ShortcutButton
+  },
+  methods: {
+    cancel() {
+      this.$emit('close')
+    },
+    confirm() {
+      this.$emit('confirm')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.full-screen {
+  background-color: rgba(0,0,0,0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.dialog-box {
+  background-color: rgba(0,0,0,0.9);
+  // width: 100%;
+  // height: 100%;
+  max-width: 130rem;
+  max-height: 42rem;
+  padding-top: 2rem;
+  padding-left: 3rem;
+  padding-right: 3rem;
+}
+
+</style>
--- src/options/controls-settings/controls-settings.vue
+++ src/options/controls-settings/controls-settings.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <p>Here, you can change keyboard shortcuts or even create your own.</p>
+    <p>Here you can create, edit or remove actions. This includes adding keyboard shortcuts that trigger them, or toggling visibility of each action in the extension popup.</p>
+    <p><b>Note that this section is highly experimental.</b> If there's extension-breaking bugs, please file a bug report on github or e-mail me.
+    However, there's some bugs that I'm aware of, but have little intentions to fix any time soon: <ul>
+      <li><b>Crop actions</b> (and maybe some others) are limited to the <i>page</i> scope (‘Video settings’ in popup) by design.
+      Yes, 'global' and 'site' checkboxes are still there, but they won't do anything. I probably won't remove them any time soon,
+      because I'd honestly rather spend my finite time on other things.</li>
+      <li>UI looks absolutely atrocious, and it's prolly gonna stay that way for a while. I am open to specific design suggestions, though.</li>
+      <li><b>For shortcuts:</b> you can use modifier keys (ctrl/alt/meta/shift), but remember to release modifier keys last. For example,
+      if you want to do use shift-S for anything: press both keys, release S, release shift. If you release shift first, extension
+      will think you were holding only shift key. You're prolly used to doing things that way already. If you're not — bad news,
+      reworking how keyboard shortcuts work isn't going to happen for a while.</li>
+      <li>I don't think reordering (in a user-friendly way at least) is gonna happen any time soon.</li>
+    </ul>
+    </p>
+
+    <p>&nbsp;</p>
+
+    <div class="flex flex-row button-box">
+      <Button label="Add new action"
+              @click.native="addAction()"
+      >
+      </Button>
+    </div>
 
     <div class="flex flex-column">
       <div class="action-item-category-header">
@@ -11,6 +33,7 @@
                    :key="index"
                    :action="action"
                    @edit="changeShortcut(index)"
+                   @remove="removeAction(index)"
         >
         </ActionAlt>
       </template>
@@ -24,6 +47,7 @@
                    :key="index"
                    :action="action"
                    @edit="changeShortcut(index)"
+                   @remove="removeAction(index)"
         >
         </ActionAlt>
       </template>
@@ -37,6 +61,7 @@
                    :key="index"
                    :action="action"
                    @edit="changeShortcut(index)"
+                   @remove="removeAction(index)"
         >
         </ActionAlt>
       </template>
@@ -55,6 +80,7 @@
                    :key="index"
                    :action="action"
                    @edit="changeShortcut(index)"
+                   @remove="removeAction(index)"
         >
         </ActionAlt>
       </template>
@@ -76,6 +102,7 @@
                    :key="index"
                    :action="action"
                    @edit="changeShortcut(index)"
+                   @remove="removeAction(index)"
         >
         </ActionAlt>
       </template>
@@ -114,6 +141,12 @@ export default {
     }
   },
   methods: {
+    removeAction(index) {
+      this.$emit('remove-event', index)
+    },
+    addAction() {
+      this.$emit('edit-event', -1);
+    },
     changeShortcut(index) {
       this.$emit('edit-event', index);
     }
