bd7bc5f53acf6ceb07c4cbf9ae0ded2f4c29cc4a
Tamius Han <ziga.pirih@hotmail.com>
2019-04-13 00:19:19 +0200

    Typo fix

--- src/options/about.vue
+++ src/options/about.vue
@@ -18,7 +18,7 @@
     <h2>Plans for the future</h2>
     <p>Improving automatic detection, trying to re-implement in-player user interface (confirmed!), fixing bugs. Maybe even give the settings page a facelift.</p>
     <h2>Acknowledgements</h2>
-    <p>This extension uses font <a href="http://overpassfont.org/">Overpass</a> and everything <a href="https://github.com/Kocal/vue-web-extension">this Vue template brings along.</p> 
+    <p>This extension uses font <a href="http://overpassfont.org/">Overpass</a> and everything <a href="https://github.com/Kocal/vue-web-extension">this Vue template brings along</a>.</p> 
 
     <h2>Special thanks to</h2>
     <ul>
