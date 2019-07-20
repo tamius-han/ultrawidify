f41ffb315511095ea88ea2ff1f6477b04f794d98
Tamius Han <tamius.han@gmail.com>
2019-07-20 21:33:15 +0200

    Readme updates for 4.3.0, version bump

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,17 +2,17 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/xternal7/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">4.2.2</p>
+    <p class="label">4.3.0</p>
+    This update mostly contains things that have the potential to make my life easier when solving issues.
     <ul>
-      <li>Fixed alignment issues for reddit on videos from v.reddit</li>
+      <li>User-friendly way of importing-exporting settings (exporting settings requires 'download' permission)</li>
+      <li>Reworked logging (basic). Logging can be enabled at runtime and and logs saved to a file. (Saving logs requires 'download' permission)</li>
       <li>Some people reported issues with inconsistent video alignment on youtube. While I've not been able to make that bug happen to me,
         (which means I haven't been able to fix it either), reports describe behaviour similar to what was going on with Iridium. Examining
         the Iridium issue revealed an issue that could be potentially blamed for this behaviour. That issue was fixed. Since I've never been able to make this problem
         happen to me, I'm not being able to verify whether that issue is gone. <b>If you're still experiencing issues with inconsistent video alignment,
           please contact me via github, reddit or email.</b> See 'Report a problem' tab for more details.</li>
     </ul>
-
-    
   </div>
 </template>
 <script>
