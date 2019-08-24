d9c409bf979cb87f61142b19ad6969db90afd830
Tamius Han <ziga.pirih@hotmail.com>
2019-08-24 22:36:24 +0200

    Update what's new

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -4,14 +4,26 @@
     <p>Full changelog for older versions <a href="https://github.com/xternal7/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
     <p class="label">4.2.3</p>
     <ul>
-      <li>Fixed twitchy behaviour on Twitch, Facebook and Twatter.</li>
+      <li>Fixed twitchy behaviour on Twitch, Facebook and Twatter. Here's a <a href="https://stuff.tamius.net/sacred-texts/2019/08/24/ultrawidify-the-twitchy-twitch-problem/" target="_blank">blog post</a> that covers the issue in more detail.</li>
+      <li>Cropping now uses user styles (as opposed to modifying element's style attribute)</li>
+      <li>Fixed the issue where one-pixel letterbox would result in constant aspect ratio corrections.</li>
+      <li>Started using mutation observers to watch for anything modifying the size of our video.</li>
     </ul>
     <p>As you can tell, I don't leave reddit and youtube much. To be fair, the twitching issue was intermittent on twitch.</p>
+    <p v-if="BrowserDetect.Chrome"><b>Chrome users:</b> as a result of Chrome's shortcomings, there now exists one potential performance issue.
+    If you notice any performance issues, please contact me via github, email or reddit (see: 'report a problem' tab
+    of this popup).</p>
   </div>
 </template>
 <script>
-export default {
+import BrowserDetect from '../../ext/conf/BrowserDetect';
 
+export default {
+  data () {
+    return {
+      BrowserDetect: BrowserDetect
+    }
+  }
 }
 </script>
 
