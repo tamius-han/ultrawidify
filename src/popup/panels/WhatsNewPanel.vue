f94331126bf3c92fd724e926e7680ae5f129abe2
Tamius Han <ziga.pirih@hotmail.com>
2019-08-25 21:21:54 +0200

    readme update

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -7,11 +7,7 @@
     <ul>
       <li>User-friendly way of importing-exporting settings (exporting settings requires 'download' permission)</li>
       <li>Reworked logging (basic). Logging can be enabled at runtime and and logs saved to a file. (Saving logs requires 'download' permission)</li>
-      <li>Some people reported issues with inconsistent video alignment on youtube. While I've not been able to make that bug happen to me,
-        (which means I haven't been able to fix it either), reports describe behaviour similar to what was going on with Iridium. Examining
-        the Iridium issue revealed an issue that could be potentially blamed for this behaviour. That issue was fixed. Since I've never been able to make this problem
-        happen to me, I'm not being able to verify whether that issue is gone. <b>If you're still experiencing issues with inconsistent video alignment,
-          please contact me via github, reddit or email.</b> See 'Report a problem' tab for more details.</li>
+      <li>Started using mutation observers to watch for changes in player size as well.</li>
     </ul>
   </div>
 </template>
