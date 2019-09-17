d2c4e77b9eeddd2b23bf568b49d8750cecfd4b69
Tamius Han <ziga.pirih@hotmail.com>
2019-09-17 22:26:43 +0200

    Version upgrade stuff

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -3,11 +3,16 @@
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/xternal7/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
     <p class="label">4.3.0</p>
-    This update mostly contains things that have the potential to make my life easier when solving issues.
     <ul>
+      <li>Fixed an issue where videos would get incorrectly aligned following a window resize.</li>
       <li>User-friendly way of importing-exporting settings (exporting settings requires 'download' permission)</li>
-      <li>Reworked logging (basic). Logging can be enabled at runtime and and logs saved to a file. (Saving logs requires 'download' permission)</li>
       <li>Started using mutation observers to watch for changes in player size as well.</li>
+      <li>Mutation observers are now used to detect both video and player size changes.</li>
+      <li>Settings patching has been reworked. Settings can now be patched incrementally.</li>
+    </ul>
+    <p>I'm also laying ground for some features that will make my life easier:</p>
+    <ul>
+      <li>Reworked how logging works internally, but this point still needs some work.</li>
     </ul>
   </div>
 </template>
