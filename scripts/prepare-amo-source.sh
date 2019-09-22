6f0f387dae498515a226e9132c66728cd891a5a5
Tamius Han <ziga.pirih@hotmail.com>
2019-09-22 02:38:13 +0200

    Let's make a script that does both ff and chrome build at the same time
    + let's automate making source code zip for AMO, while we're at it

---
+++ scripts/prepare-amo-source.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# makes a zip file with human-readable source code that we need to upload to AMO
+# since webpack minifies stuff
+zip -r dist-zip/uw-amo-source.zip README-AMO.md .babelrc package.json package-lock.json webpack.config.js src/
