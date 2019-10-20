7a9c341bf821cd907462555a2645938af19c4cf2
Tamius Han <tamius.han@gmail.com>
2019-10-20 14:13:58 +0200

    Version update stuff

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,8 +2,9 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/xternal7/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">4.3.1</p>
+    <p class="label">4.3.1.1</p>
     <ul>
+      <li><b>[4.3.3.1]</b> Patched twitch.tv</li>
       <li>Minor rework of settings page (actions & shortcuts section)</li>
       <li>Fixed bug that prevented settings page from opening</li>
     </ul>
