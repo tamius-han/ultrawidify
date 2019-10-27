d5b02b76f0eb28222c05618aa86e2f6094c7ef7d
Tamius Han <ziga.pirih@hotmail.com>
2019-10-27 23:20:45 +0100

    Fix popup not displaying

--- src/common/mixins/ComputeActionsMixin.js
+++ src/common/mixins/ComputeActionsMixin.js
@@ -1,7 +1,13 @@
 export default {
   computed: {
     scopeActions: function() {
-      return this.settings.active.actions.filter(x => x.scopes[this.scope] && x.scopes[this.scope].show) || [];
+      return this.settings.active.actions.filter(x => {
+        if (! x.scopes) {
+          console.error('This action does not have a scope.', x);
+          return false;
+        }
+        return x.scopes[this.scope] && x.scopes[this.scope].show
+      }) || [];
     },
     extensionActions: function(){
       return this.scopeActions.filter(x => x.cmd.length === 1 && x.cmd[0].action === 'set-extension-mode') || [];
