f949decdf05f1efada1d1f569204bd5d89670244
Tamius Han <ziga.pirih@hotmail.com>
2020-02-26 00:43:01 +0100

    Some stuff for easier logging configuration - wip

---
+++ src/common/components/JsonExplorer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="root">
+    <template v-for="key in json"
+    >
+      <div class="element" :key="key">
+        <span class="json-key">"{{key}}"</span>:
+          <template v-if="Array.isArray(json[key])">
+            [<br/>
+              <div v-for="index of json[key]"
+                   class="json-table-row"
+                   :key="index"
+              >
+              
+              </div>
+            ]
+          </template>
+      </div>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  name: "json-explorer",
+  props: {
+    json: Object
+  }
+}
+</script>
