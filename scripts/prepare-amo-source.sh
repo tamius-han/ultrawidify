157da86d6bb3b7c7356b384638c247d985751c59
Tamius Han <ziga.pirih@hotmail.com>
2020-06-14 21:48:04 +0200

    Fix script that packs source code for AMO

--- scripts/prepare-amo-source.sh
+++ scripts/prepare-amo-source.sh
@@ -2,4 +2,4 @@
 
 # makes a zip file with human-readable source code that we need to upload to AMO
 # since webpack minifies stuff
-zip -r dist-zip/uw-amo-source.zip README-AMO.md .babelrc package.json package-lock.json webpack.config.js src/
+zip -r dist-zip/uw-amo-source.zip README-AMO.md .babelrc package.json package-lock.json webpack.config.js scripts src/
