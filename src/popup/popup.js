e56551c79dde7c0cae4ca74b3dd623769fb9ba59
Tamius Han <tamius.han@gmail.com>
2020-12-01 00:22:22 +0100

    Remove redundant code

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App';
 
-// global.browser = require('webextension-polyfill')
-// Vue.prototype.$browser = global.browser
-
 createApp(App).mount('#app');
