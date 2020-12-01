e10063b978010f649d215d611c0fead4da8f8c08
Tamius Han <ziga.pirih@hotmail.com>
2020-12-01 01:48:52 +0100

    Update changelog with stuff that was done today

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,22 +2,19 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/tamius-han/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">4.4.10</p>
+    <p class="label">4.5.0</p>
     <ul>
       <li>
-        Video alignment should now work on Twitch (<a href="https://github.com/tamius-han/ultrawidify/issues/109">#109</a>)
+        Under the hood: migrated from vue2 to vue3, because optional chaining in templates is too OP.
       </li>
       <li>
-        Videos should now align properly on Hulu while cropped (<a href="https://github.com/tamius-han/ultrawidify/issues/111">#111 &amp; via email</a>)
+        (On options page, section 'Action &amp; shortcuts') Manual aspect ratio now supports entering custom ratios using '21/9' and '2.39:1' formats (as opposed to single number, e.g. '2.39') — <a href="https://github.com/tamius-han/ultrawidify/issues/121">#121</a>.
       </li>
       <li>
-        Fixed a problem where changing certain settings would cause multiple instances of Ultrawidify to run on a page, effectively preventing some crop options to be set until reload. (possibly <a href="(https://github.com/tamius-han/ultrawidify/issues/112">#112</a>?)
-      </li>
-       <li>
-        Fixed a problem where embedded videos would be misaligned after switching from full screen
+        Added config for wakanim.tv (special thanks to <a href="https://github.com/saschanaz">@saschanaz</a> for doing the legwork — <a href="https://github.com/tamius-han/ultrawidify/issues/113">#113</a>)
       </li>
       <li>
-        <b>[4.4.10.1]</b> Fixed cruncyhroll regression — <a href="https://github.com/tamius-han/ultrawidify/issues/115">#115</a>
+        (In Firefox) When extension was placed in overflow menu, the popup was cut off. That should be fixed now. (<a href="https://github.com/tamius-han/ultrawidify/issues/119">#119</a>)
       </li>
     </ul>
   </div>
