a91ed7368f2a3f4ce610bf76de185dec518e4eaf
Tamius Han <tamius.han@gmail.com>
2020-12-02 22:39:43 +0100

    Add icon component

---
+++ src/common/components/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg class="bi" width="1em" height="1em" fill="currentColor">
+    <use :xlink:href=" '' + iconHref" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+  },
+  data() {
+    return {
+      bootstrapIconsBasePath: (browser ?? chrome).runtime.getURL('/res/icons/bootstrap-icons.svg'),
+      iconHref: '',
+    };
+  },
+  created() {
+    this.iconHref = `${this.bootstrapIconsBasePath}#${this.icon}`;
+  }
+}
+</script>
+
+<style scoped>
+</style>
