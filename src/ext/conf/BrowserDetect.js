7cb378029cf88068a2a5296c739b73741d031897
Tamius Han <tamius.han@gmail.com>
2020-12-30 15:35:54 +0100

    Some cosmetic changes in BrowserDetect

--- src/ext/conf/BrowserDetect.js
+++ src/ext/conf/BrowserDetect.js
@@ -4,6 +4,17 @@ if (process.env.CHANNEL !== 'stable') {
   console.info('Loaded BrowserDetect');
 }
 
+function detectEdgeUA() {
+  return /Edg\/(\.?[0-9]*)*$/.test(window.navigator.userAgent);
+}
+
+function getBrowserObj() {
+  return process.env.BROWSER === 'firefox' ? browser : chrome;
+}
+
+function getURL(url) {
+  return process.env.BROWSER === 'firefox' ? browser.runtime.getURL(url) : chrome.runtime.getURL(url);
+}
 
 const BrowserDetect = {
   firefox: process.env.BROWSER === 'firefox',
@@ -12,9 +23,9 @@ const BrowserDetect = {
   edge: process.env.BROWSER === 'edge',
   processEnvBrowser: process.env.BROWSER,
   processEnvChannel: process.env.CHANNEL,
-  isEdgeUA: () => /Edg\/(\.?[0-9]*)*$/.test(window.navigator.userAgent),
-  getBrowserObj: () => { return process.env.BROWSER === 'firefox' ? browser : chrome; },
-  getURL: (url) => { console.log('getting file:', url); console.log(process.env.BROWSER === 'firefox' ? browser.runtime.getURL(url) : chrome.runtime.getURL(url)); return process.env.BROWSER === 'firefox' ? browser.runtime.getURL(url) : chrome.runtime.getURL(url); },
+  isEdgeUA: detectEdgeUA(),
+  browserObj: getBrowserObj(),
+  getURL: (url) => getURL(url),
 } 
 
 if (process.env.CHANNEL !== 'stable') {
