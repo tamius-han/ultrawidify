df2b0f56145d588c44ffd693152a2df2e3aadc63
Tamius Han <tamius.han@gmail.com>
2021-01-12 23:18:14 +0100

    Add toString method to aspect ratio enum

--- src/common/enums/aspect-ratio.enum.js
+++ src/common/enums/aspect-ratio.enum.js
@@ -6,6 +6,19 @@ var AspectRatio = Object.freeze({
   FitHeight: 3,   // legacy/dynamic: fit to height
   Fixed: 4,       // pre-determined aspect ratio
   Manual: 5,      // ratio achieved by zooming in/zooming out
+
+  toString: (ar) => {
+    switch (ar) {
+      case -1: return 'Initial';
+      case 0:  return 'Reset';
+      case 1:  return 'Automatic';
+      case 2:  return 'FitWidth';
+      case 3:  return 'FitHeight';
+      case 4:  return 'Fixed';
+      case 5:  return 'Manual';
+      default: return '<not an valid enum value>'
+    }
+  }
 });
 
 export default AspectRatio;
