1ca6c7dad4b3028070d6e3746892ba8a9fa65b68
Tamius Han <ziga.pirih@hotmail.com>
2021-02-18 22:29:23 +0100

    convert ArDetector to ts

--- src/ext/lib/ar-detect/GuardLine.ts
+++ src/ext/lib/ar-detect/GuardLine.ts
@@ -1,13 +1,33 @@
 import Debug from '../../conf/Debug';
+import Settings from '../Settings';
+import ArDetector from './ArDetector';
+
+export type GuardLineBar = {
+  top?: number;
+  bottom?: number;
+}
+
+export type ImageCheckResult = {
+  success: boolean;
+}
 
 class GuardLine {
-  
+  blackbar: GuardLineBar;
+  imageBar: GuardLineBar;
+
+  aard: ArDetector;
+  settings: Settings;
+
+  blackbarThreshold: number;
+  imageThreshold: number;
+
+
   // ardConf — reference to ArDetector that has current GuardLine instance
-  constructor(ardConf){
+  constructor (ardConf){
     this.blackbar = {top: undefined, bottom: undefined};
     this.imageBar = {top: undefined, bottom: undefined};
     
-    this.conf = ardConf;
+    this.aard = ardConf;
     this.settings = ardConf.settings;
   }
 
@@ -33,7 +53,7 @@ class GuardLine {
 
     // to odstrani vse neveljavne nastavitve in vse možnosti, ki niso smiselne
     // this removes any configs with invalid values or values that dont make sense
-    if (bbTop < 0 || bbBottom >= this.conf.canvas.height ){
+    if (bbTop < 0 || bbBottom >= this.aard.canvas.height ){
       throw {error: "INVALID_SETTINGS_IN_GUARDLINE", bbTop, bbBottom}
     }
 
@@ -51,7 +71,7 @@ class GuardLine {
   check(image, fallbackMode){
     // izračunaj enkrat in shrani na objekt
     // calculate once and save object-instance-wide
-    this.blackbarThreshold = this.conf.blackLevel + this.settings.active.arDetect.blackbar.threshold;
+    this.blackbarThreshold = this.aard.blackLevel + this.settings.active.arDetect.blackbar.threshold;
     this.imageThreshold = this.blackbarThreshold + this.settings.active.arDetect.blackbar.imageThreshold;
 
     // dejansko testiranje
@@ -95,7 +115,7 @@ class GuardLine {
       return { success: true };
     }
 
-    var offset = parseInt(this.conf.canvas.width * this.settings.active.arDetect.guardLine.ignoreEdgeMargin) << 2;
+    var offset = (this.aard.canvas.width * this.settings.active.arDetect.guardLine.ignoreEdgeMargin) << 2;
     
     var offenders = [];
     var offenderCount = -1; // doing it this way means first offender has offenderCount==0. Ez index.
@@ -112,28 +132,28 @@ class GuardLine {
     else{
       // fallback mode is a bit different
       edge_upper = 0;
-      edge_lower = this.conf.canvas.height - 1;
+      edge_lower = this.aard.canvas.height - 1;
     }
     
     var rowStart, rowEnd;
     
     // <<<=======| checking upper row |========>>>
     
-    rowStart = ((edge_upper * this.conf.canvas.width) << 2) + offset;
-    rowEnd = rowStart + ( this.conf.canvas.width << 2 ) - (offset * 2);
+    rowStart = ((edge_upper * this.aard.canvas.width) << 2) + offset;
+    rowEnd = rowStart + ( this.aard.canvas.width << 2 ) - (offset * 2);
     
     if (Debug.debugCanvas.enabled && Debug.debugCanvas.guardLine) {
-      offenderCount = this._gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount);
+      // offenderCount = this._gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount);
     } else {
       offenderCount = this._gl_rowCheck(image, rowStart, rowEnd, offenders, offenderCount);    
     }
     // <<<=======| checking lower row |========>>>
     
-    rowStart = ((edge_lower * this.conf.canvas.width) << 2) + offset;
-    rowEnd = rowStart + ( this.conf.canvas.width << 2 ) - (offset * 2);
+    rowStart = ((edge_lower * this.aard.canvas.width) << 2) + offset;
+    rowEnd = rowStart + ( this.aard.canvas.width << 2 ) - (offset * 2);
     
     if (Debug.debugCanvas.enabled && Debug.debugCanvas.guardLine) {
-      offenderCount = this._gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount);
+      // offenderCount = this._gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount);
     } else {
       offenderCount = this._gl_rowCheck(image, rowStart, rowEnd, offenders, offenderCount);    
     }
@@ -156,11 +176,11 @@ class GuardLine {
     return {success: false, offenders: ret};
   }
 
-  imageCheck(image){  
+  imageCheck(image, fallbackMode?: boolean): ImageCheckResult {  
     if(!this.imageBar.top || !this.imageBar.bottom)
       return { success: false };
         
-    var offset = (this.conf.canvas.width * this.settings.active.arDetect.guardLine.ignoreEdgeMargin) << 2;
+    var offset = (this.aard.canvas.width * this.settings.active.arDetect.guardLine.ignoreEdgeMargin) << 2;
       
     // TODO: implement logo check.
     
@@ -181,35 +201,35 @@ class GuardLine {
     // robu (eden izmed robov je lahko v celoti črn)
     // how many non-black pixels we need to consider this check a success. We only need to detect enough pixels
     // on one edge (one of the edges can be black as long as both aren't)
-    var successThreshold = (this.conf.canvas.width * this.settings.active.arDetect.guardLine.imageTestThreshold);
+    var successThreshold = (this.aard.canvas.width * this.settings.active.arDetect.guardLine.imageTestThreshold);
     var rowStart, rowEnd;
     
     
     // <<<=======| checking upper row |========>>>
     
-    rowStart = ((edge_upper * this.conf.canvas.width) << 2) + offset;
-    rowEnd = rowStart + ( this.conf.canvas.width << 2 ) - (offset * 2);
+    rowStart = ((edge_upper * this.aard.canvas.width) << 2) + offset;
+    rowEnd = rowStart + ( this.aard.canvas.width << 2 ) - (offset * 2);
     
     var res = false;
     
     if(Debug.debugCanvas.enabled && Debug.debugCanvas.guardLine){
-      res = this._ti_debugCheckRow(image, rowStart, rowEnd, successThreshold);
+      // res = this._ti_debugCheckRow(image, rowStart, rowEnd, successThreshold);
     } else {
       res = this._ti_checkRow(image, rowStart, rowEnd,successThreshold);
     }
     
-    if(res)
+    if (res) {
       return {success: true};
-    
+    }
     
     // <<<=======| checking lower row |========>>>
     
-    rowStart = ((edge_lower * this.conf.canvas.width) << 2) + offset;
+    rowStart = ((edge_lower * this.aard.canvas.width) << 2) + offset;
     // rowEnd = rowStart + ( this.conf.canvas.width << 2 ) - (offset * 2);
     
 
     if(Debug.debugCanvas.enabled && Debug.debugCanvas.guardLine){
-      res = this._ti_debugCheckRow(image, rowStart, rowEnd, successThreshold);
+      // res = this._ti_debugCheckRow(image, rowStart, rowEnd, successThreshold);
     } else {
       res = this._ti_checkRow(image, rowStart, rowEnd,successThreshold);
     }
@@ -246,35 +266,35 @@ class GuardLine {
     return offenderCount;
   }
 
-  _gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount){
-    var firstOffender = -1;
-    for(var i = rowStart; i < rowEnd; i+=4){
+  // _gl_debugRowCheck(image, rowStart, rowEnd, offenders, offenderCount){
+  //   var firstOffender = -1;
+  //   for(var i = rowStart; i < rowEnd; i+=4){
       
-      // we track sections that go over what's supposed to be a black line, so we can suggest more 
-      // columns to sample
-      if(image[i] > this.blackbarThreshold || image[i+1] > this.blackbarThreshold || image[i+2] > this.blackbarThreshold){
-        this.conf.debugCanvas.trace(i, DebugCanvasClasses.VIOLATION);      
-        if(firstOffender < 0){
-          firstOffender = (i - rowStart) >> 2;
-          offenderCount++;
-          offenders.push({x: firstOffender, width: 1});
-        }
-        else{
-          offenders[offenderCount].width++
-        }
-      }
-      else{
-        this.conf.debugCanvas.trace(i, DebugCanvasClasses.GUARDLINE_BLACKBAR);              
-        // is that a black pixel again? Let's reset the 'first offender' 
-        firstOffender = -1;
-      }
+  //     // we track sections that go over what's supposed to be a black line, so we can suggest more 
+  //     // columns to sample
+  //     if(image[i] > this.blackbarThreshold || image[i+1] > this.blackbarThreshold || image[i+2] > this.blackbarThreshold){
+  //       this.aard.debugCanvas.trace(i, DebugCanvasClasses.VIOLATION);      
+  //       if(firstOffender < 0){
+  //         firstOffender = (i - rowStart) >> 2;
+  //         offenderCount++;
+  //         offenders.push({x: firstOffender, width: 1});
+  //       }
+  //       else{
+  //         offenders[offenderCount].width++
+  //       }
+  //     }
+  //     else{
+  //       this.aard.debugCanvas.trace(i, DebugCanvasClasses.GUARDLINE_BLACKBAR);              
+  //       // is that a black pixel again? Let's reset the 'first offender' 
+  //       firstOffender = -1;
+  //     }
       
-    }
+  //   }
   
-    return offenderCount;
-  }
+  //   return offenderCount;
+  // }
 
-  _ti_checkRow(image, rowStart, rowEnd, successThreshold) {
+  _ti_checkRow(image, rowStart, rowEnd, successThreshold): boolean {
     for(var i = rowStart; i < rowEnd; i+=4){
       if(image[i] > this.imageThreshold || image[i+1] > this.imageThreshold || image[i+2] > this.imageThreshold){
         if(successThreshold --<= 0){
@@ -286,20 +306,20 @@ class GuardLine {
     return false;
   }
 
-  _ti_debugCheckRow(image, rowStart, rowEnd, successThreshold) {
-    for(var i = rowStart; i < rowEnd; i+=4){
-      if(image[i] > this.imageThreshold || image[i+1] > this.imageThreshold || image[i+2] > this.imageThreshold){
-        this.conf.debugCanvas.trace(i, DebugCanvasClasses.GUARDLINE_IMAGE);
-        if(successThreshold --<= 0){
-          return true;
-        }
-      } else {
-        this.conf.debugCanvas.trace(i, DebugCanvasClasses.WARN);
-      }   
-    }
+  // _ti_debugCheckRow(image, rowStart, rowEnd, successThreshold) {
+  //   for(var i = rowStart; i < rowEnd; i+=4){
+  //     if(image[i] > this.imageThreshold || image[i+1] > this.imageThreshold || image[i+2] > this.imageThreshold){
+  //       this.aard.debugCanvas.trace(i, DebugCanvasClasses.GUARDLINE_IMAGE);
+  //       if(successThreshold --<= 0){
+  //         return true;
+  //       }
+  //     } else {
+  //       this.aard.debugCanvas.trace(i, DebugCanvasClasses.WARN);
+  //     }   
+  //   }
   
-    return false;
-  }
+  //   return false;
+  // }
 }
 
 export default GuardLine;
