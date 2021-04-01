09253fe0a19f9cf3bb400c3e0de2b0b3389edb0a
Tamius Han <tamius.han@gmail.com>
2021-04-01 21:40:46 +0200

    Unbork settings page even more

--- src/common/components/ActionAlt.vue
+++ src/common/components/ActionAlt.vue
@@ -117,7 +117,7 @@ export default {
   data () {
     return {
       StretchType: StretchType,
-      AspectRatio: AspectRatio,
+      AspectRatioType: AspectRatioType,
     }
   },
   created () {
