965a48c0867e6b4b8ebca392a828488da83293a4
Tamius Han <tamius.han@gmail.com>
2021-04-12 20:54:42 +0200

    Remove some code that was only needed cos old edge

--- src/ext/uw.js
+++ src/ext/uw.js
@@ -2,9 +2,7 @@
  * NOTE: we cannot get rid of this js file. I tried for 30 seconds and I couldn't get 
  * extension to work unless I kept this part of extension out of the ts file.
  */
-
 import UWContent from './UWContent';
-import BrowserDetect from './conf/BrowserDetect';
 
 if(process.env.CHANNEL !== 'stable'){
   console.warn("\n\n\n\n\n\n           ———    Sᴛλʀᴛɪɴɢ  Uʟᴛʀᴀᴡɪᴅɪꜰʏ    ———\n               <<   ʟᴏᴀᴅɪɴɢ ᴍᴀɪɴ ꜰɪʟᴇ   >>\n\n\n\n");
@@ -18,10 +16,6 @@ if(process.env.CHANNEL !== 'stable'){
   } catch (e) {
     console.info("%cWe are in an iframe!", "color: #fea, background: #d31");
   }
-}
-
-if (BrowserDetect.edge) {
-  HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 }
 
 const main = new UWContent();
