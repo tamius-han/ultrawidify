cbaa424fc4c35ded09d9066e2a62e0aa0a51e548
Tamius Han <tamius.han@gmail.com>
2021-06-07 22:16:03 +0200

    Version bumps and changelogs

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,16 +2,12 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/tamius-han/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">5.0.3</p>
+    <p class="label">5.0.4</p>
     <ul>
       <li>
-        Fixed the issue where the videos were sometimes offset up and left. Again.
-      </li>
-      <li>
-        Fix the issue where correcting source stretch was squished incorrectly (<a href="https://github.com/tamius-han/ultrawidify/issues/153">#153</a>)
+        Updated config for disney+. Hopefully it works now. Special thanks to <a href="https://github.com/tamius-han/ultrawidify/issues/84#issuecomment-846334005" target="_blank">@jwannebo</a>.
       </li>
     </ul>
-    <p>
       <small><b>NOTE:</b> zoom limitations were introduced as a workaround for a bug caused by Chrome's/Edge's faulty hardware acceleration
       which caused videos to be incorrectly stretched on nVidia GPUs while in fullscreen. While this issue is reportedly fixed as
       of few weeks ago, a few people reported having this issue within the past two weeks.</small>
