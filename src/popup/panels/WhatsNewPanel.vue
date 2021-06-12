c43b1daa4268e3fbc285a7db5f6fca5acbe3944f
Tamius Han <tamius.han@gmail.com>
2021-06-12 23:19:19 +0200

    Edge probably doesn't need the 'pls ensure you're up to date' warning anymore

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -8,9 +8,8 @@
         Updated config for disney+. Hopefully it works now. Special thanks to <a href="https://github.com/tamius-han/ultrawidify/issues/84#issuecomment-846334005" target="_blank">@jwannebo</a>.
       </li>
     </ul>
-      <small><b>NOTE:</b> zoom limitations were introduced as a workaround for a bug caused by Chrome's/Edge's faulty hardware acceleration
-      which caused videos to be incorrectly stretched on nVidia GPUs while in fullscreen. While this issue is reportedly fixed as
-      of few weeks ago, a few people reported having this issue within the past two weeks.</small>
+    <p>
+      <small><b>NOTE from older versions:</b> zoom limitations were introduced as a workaround for a bug caused by Chrome's/Edge's faulty hardware acceleration.</small>
     </p>
     <p>
       <small>If you experience issues with videos being stretched incorrectly at certain zoom levels, go to:</small><br/>
