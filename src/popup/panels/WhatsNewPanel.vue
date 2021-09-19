cda6b4e10d708fee6ce5d731948f690710244c28
Tamius Han <tamius.han@gmail.com>
2021-09-19 21:32:47 +0200

    Version bump n shit

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,20 +2,23 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/tamius-han/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">5.1.0</p>
+    <p class="label">5.1.1</p>
+    <p>
+      In last patch notes:
+    </p>
+    <pre>
+> Hopefully that didn't break anything too much
+    </pre>
+    <p>
+      Well, about that ...
+    </p>
     <ul>
       <li>
-        Under the hood changes: aspect ratio autodetection now uses requestAnimationFrame instead of a setTimeout/setInterval-based loop.
-      </li>
-      <li>
-        Logger is sorta fixed.
+        Fixed the problem where autodetection wouldn't work for most people that aren't me.
       </li>
     </ul>
-    <p>
-      Hopefully that didn't break anything too much.
-    </p>
     <p><small>
-      <b>Known issues:</b> zooming is limited in Chromium-based browsers. This is a browser bug that no extension can fix. See <a href="https://github.com/tamius-han/ultrawidify/discussions/161" target="_blank">this</a> for more info.
+      <b>Known issues:</b> zooming is limited in Chromium-based browsers. This is a browser bug that no extension can fix. Technical details about this can be found <a href="https://stuff.tamius.net/sacred-texts/2021/08/19/ultrawidify-and-chrome-2021-edition-episode-2/" target="_blank">on my blog.</a>.
     </small></p>
   </div>
 </template>
