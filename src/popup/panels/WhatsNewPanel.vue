e0fe4c7103d14c1740210b6f83706e6b2b3d600e
Tamius Han <tamius.han@gmail.com>
2022-08-06 01:56:12 +0200

    Version bump + changelog

--- src/popup/panels/WhatsNewPanel.vue
+++ src/popup/panels/WhatsNewPanel.vue
@@ -2,18 +2,10 @@
   <div>
     <h2>What's new</h2>
     <p>Full changelog for older versions <a href="https://github.com/tamius-han/ultrawidify/blob/master/CHANGELOG.md">is available here</a>.</p>
-    <p class="label">5.1.3</p>
+    <p class="label">5.1.4</p>
+    <small><sup>By Smodur's tail, this "let's respect people's CPUs and NOT call <code>drawImage()</code> / <code>getImageData()</code> twice when we don't have to" update is low key <i>not</i> going well.</sup></small>
     <ul>
-      <li>Fixed some problems with autodetection sometimes briefly resetting on dark frames (<a href="https://github.com/tamius-han/ultrawidify/issues/195" target="_blank">#195</a>), <a href="https://github.com/tamius-han/ultrawidify/issues/196" target="_blank">#196</a>)</li>
-    </ul>
-    <p class="label">5.1.2</p>
-    <ul>
-      <li>
-        Fixed the issue where "Enable extension (for this site)" option was not visible in the extension popup.
-      </li>
-      <li>
-        Blackframe tests now run on same data as main algorithm as opposed on a smaller sample (<code>drawImage()</code> calls are <i>very</i> expensive even for a 16x9 sample).
-      </li>
+      <li>Fixed some problems with autodetection not returning to 16:9 when necessary if autodetection already changed aspect ratio (<a href="https://github.com/tamius-han/ultrawidify/issues/198" target="blank">#198</a>)</li>
     </ul>
   </div>
 </template>
