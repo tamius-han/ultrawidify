5d389a07966b312da3a94c0ecf3ffd8117fc8d90
Tamius Han <tamius.han@gmail.com>
2023-01-06 18:22:15 +0100

    Rename a file for future consistency

--- src/csui/csui.js
+++ src/csui/csui.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue';
-import PlayerUiBase from './PlayerUiBase';
+import PlayerOverlay from './PlayerOverlay';
 import mdiVue from 'mdi-vue/v3';
 import * as mdijs from '@mdi/js';
 
 // NOTE — this is in-player interface for ultrawidify
 // it is injected into the page in UI.init()
 
-createApp(PlayerUiBase)
+createApp(PlayerOverlay)
   .use(mdiVue, {icons: mdijs})
   .mount('#app');
