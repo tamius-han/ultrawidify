7a1e8be48a5d6dce6db27f64cb7984b8f5c05c85
Tamius Han <tamius.han@gmail.com>
2024-12-22 23:16:54 +0100

    attempt to shut some warnings up (but fail)

--- webpack.config.js
+++ webpack.config.js
@@ -203,7 +203,8 @@ const config = {
       'process.env.CHANNEL': JSON.stringify(process.env.CHANNEL),
 
       '__VUE_OPTIONS_API__': true,
-      '__VUE_PROD_DEVTOOLS__': false
+      '__VUE_PROD_DEVTOOLS__': false,
+      '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': true
     })
   ],
   optimization: {
