6ea47d26d0dea93b686ec20831b347d8ac2e8cb3
Tamius Han <tamius.han@gmail.com>
2025-01-28 00:42:56 +0100

    remove cta from UI advertisement

--- src/csui/src/PlayerUiPanels/InPlayerUiAdvertisement.vue
+++ src/csui/src/PlayerUiPanels/InPlayerUiAdvertisement.vue
@@ -10,26 +10,6 @@
       Show settings window
     </div>
 
-    <div>
-      <b>Is your screen entirely white or entirely black?</b>
-
-      <p>This appears to be a rare issue that happens to some people. If you're experiencing this issue, please consider contacting me and sharing the following data:</p>
-
-      <ul>
-        <li>Which sites this problem appears on and whether it happens on youtube. If you use youtube premium, please try signing out of youtube (or use a new profile in Google Chrome) in order to see whether youtube premium is required.</li>
-        <li>your browser. if using browsers other than Chrome, please try to reproduce this issue in Chrome</li>
-        <li>your operating system</li>
-        <li>your graphics card</li>
-        <li>the following line:<br/>
-          <pre>prefers-color-scheme dark: {{pageData.pcsDark}}; prefers-color-scheme light: {{pageData.pcsLight}}; color-scheme: {{pageData.colorScheme}}</pre>
-        </li>
-      </ul>
-
-      <p>Please post this info to <a href="https://github.com/tamius-han/ultrawidify/issues/262" target="_blank">this thread</a>, or message me via e-mail.</p>
-
-      <p>Then, disable the in-player UI.</p>
-    </div>
-
     <!-- <p></p>
     <p></p>
     <p>In-player UI should show and hide automatically as you start or stop moving your mouse inside the player window.</p>
