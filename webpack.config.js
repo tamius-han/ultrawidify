8669a834c03250ed75883808974105b1074bd60e
Tamius Han <tamius.han@gmail.com>
2025-03-19 00:42:07 +0100

    Update things

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const webpack = require('webpack');
 const ejs = require('ejs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
 const { VueLoaderPlugin } = require('vue-loader');
 const path = require('path');
 
@@ -48,7 +47,7 @@ const config = {
       },
       {
         test: /\.vue$/,
-        loaders: 'vue-loader',
+        loader: 'vue-loader',
       },
       {
         test: /\.js$/,
@@ -121,86 +120,92 @@ const config = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    new CopyWebpackPlugin([
-      { from: 'res', to: 'res', ignore: ['css', 'css/**']},
-      { from: 'ext', to: 'ext', ignore: ['conf/*', 'lib/**']},
-      { from: 'csui', to: 'csui', ignore: ['src']},
-
-      // we need to get webextension-polyfill and put it in common/lib
-      { from: '../node_modules/webextension-polyfill/dist/browser-polyfill.js', to: 'common/lib/browser-polyfill.js'},
-
-      // This is extension icon, as used on extension lists and/or extension's action button
-      // This folder does not contain any GUI icons — these are in /res/icons.
-      // (TODO: check if this copy is even necessary — /icons has same content as /res/icons)
-      { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
-      { from: 'csui/csui-popup.html', to: 'csui/csui-popup.html', transform: transformHtml },
-      { from: 'csui/csui-overlay-normal.html', to: 'csui/csui.html', transform: transformHtml },
-      { from: 'csui/csui-overlay-dark.html', to: 'csui/csui-dark.html', transform: transformHtml },
-      { from: 'csui/csui-overlay-light.html', to: 'csui/csui-light.html', transform: transformHtml },
-      // { from: 'install/first-time/first-time.html', to: 'install/first-time/first-time.html', transform: transformHtml},
+    new CopyWebpackPlugin(
       {
-        from: 'manifest.json',
-        to: 'manifest.json',
-        transform: (content) => {
-          const jsonContent = JSON.parse(content);
-          // jsonContent.version = version;
+        patterns: [
+          { from: 'res', to: 'res',   globOptions: { ignore: ['css', 'css/**']} },
+          { from: 'ext', to: 'ext',   globOptions: { ignore: ['conf/*', 'lib/**']} },
+          { from: 'csui', to: 'csui', globOptions: { ignore: ['src']} },
 
-          // if (config.mode === 'development') {
-          //   jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
-          // }
+          // we need to get webextension-polyfill and put it in common/lib
+          { from: '../node_modules/webextension-polyfill/dist/browser-polyfill.js', to: 'common/lib/browser-polyfill.js'},
 
-          if (process.env.CHANNEL === 'nightly') {
-            jsonContent.name = "Ultrawidify - nightly";
-            jsonContent.description = "FOR TESTING ONLY -- THIS BUILD USES ONLY THE FRESHEST COMMITS FROM GITHUB AND MAY THEREFORE BE COMPLETELY BROKEN";
+          // This is extension icon, as used on extension lists and/or extension's action button
+          // This folder does not contain any GUI icons — these are in /res/icons.
+          // (TODO: check if this copy is even necessary — /icons has same content as /res/icons)
+          { from: 'icons', to: 'icons', globOptions: { ignore: ['icon.xcf'] }},
+          { from: 'csui/csui-popup.html', to: 'csui/csui-popup.html', transform: transformHtml },
+          { from: 'csui/csui-overlay-normal.html', to: 'csui/csui.html', transform: transformHtml },
+          { from: 'csui/csui-overlay-dark.html', to: 'csui/csui-dark.html', transform: transformHtml },
+          { from: 'csui/csui-overlay-light.html', to: 'csui/csui-light.html', transform: transformHtml },
+          // { from: 'install/first-time/first-time.html', to: 'install/first-time/first-time.html', transform: transformHtml},
+          {
+            from: 'manifest.json',
+            to: 'manifest.json',
+            transform: (content) => {
+              const jsonContent = JSON.parse(content);
+              // jsonContent.version = version;
 
-            // version numbers for nightly builds: YYMM.DD.BUILD_NUMBER
-            jsonContent.version = `${new Date()
-                                            .toISOString()     // YYYY-MM-DDTHH:MM:SS...
-                                            .split('T')[0]     // gives YYYY-MM-DD
-                                            .substr(2)         // YYYY -> YY
-                                            .replace('-', '')  // YY-MM-DD -> YYMM-DD
-                                            .replace('-', '.') // YYMM-DD -> YYMM.DD
-                                    }.${process.env.BUILD_NUMBER === undefined ? 0 : process.env.BUILD_NUMBER}`;
-            jsonContent.browser_action.default_title = "Ultrawidify Nightly";
+              // if (config.mode === 'development') {
+              //   jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
+              // }
 
-            // because we don't want web-ext to submit this as proper release
-            delete jsonContent.applications;
-          } else if (process.env.CHANNEL === 'testing') {
-            jsonContent.name = "Ultrawidify - testing";
-            jsonContent.description = "FOR TESTING ONLY -- this build is intended for testing a fix of certain bugs. It's not fit for normal use.";
+              if (process.env.CHANNEL === 'nightly') {
+                jsonContent.name = "Ultrawidify - nightly";
+                jsonContent.description = "FOR TESTING ONLY -- THIS BUILD USES ONLY THE FRESHEST COMMITS FROM GITHUB AND MAY THEREFORE BE COMPLETELY BROKEN";
 
-            // version numbers for nightly builds: YYMM.DD.BUILD_NUMBER
-            jsonContent.version = `${new Date()
-                                            .toISOString()     // YYYY-MM-DDTHH:MM:SS...
-                                            .split('T')[0]     // gives YYYY-MM-DD
-                                            .substr(2)         // YYYY -> YY
-                                            .replace('-', '')  // YY-MM-DD -> YYMM-DD
-                                            .replace('-', '.') // YYMM-DD -> YYMM.DD
-                                    }.${process.env.BUILD_NUMBER === undefined ? 0 : process.env.BUILD_NUMBER}`;
-            jsonContent.browser_action.default_title = "Ultrawidify Testing";
+                // version numbers for nightly builds: YYMM.DD.BUILD_NUMBER
+                jsonContent.version = `${new Date()
+                                                .toISOString()     // YYYY-MM-DDTHH:MM:SS...
+                                                .split('T')[0]     // gives YYYY-MM-DD
+                                                .substr(2)         // YYYY -> YY
+                                                .replace('-', '')  // YY-MM-DD -> YYMM-DD
+                                                .replace('-', '.') // YYMM-DD -> YYMM.DD
+                                        }.${process.env.BUILD_NUMBER === undefined ? 0 : process.env.BUILD_NUMBER}`;
+                jsonContent.browser_action.default_title = "Ultrawidify Nightly";
 
-            // because we don't want web-ext to submit this as proper release
-            delete jsonContent.applications;
-          }
+                // because we don't want web-ext to submit this as proper release
+                delete jsonContent.applications;
+              } else if (process.env.CHANNEL === 'testing') {
+                jsonContent.name = "Ultrawidify - testing";
+                jsonContent.description = "FOR TESTING ONLY -- this build is intended for testing a fix of certain bugs. It's not fit for normal use.";
 
-          if (process.env.BROWSER !== 'firefox') {
-            jsonContent.version = jsonContent.version.replace(/[a-zA-Z-]/g, '');
-            try {
-              delete jsonContent.options_ui.browser_style;
-            } catch (e) { }
-            try {
-              delete jsonContent.background.scripts;
-            } catch (e) {}
-          } else {
-            delete jsonContent.background.service_worker;
-          }
+                // version numbers for nightly builds: YYMM.DD.BUILD_NUMBER
+                jsonContent.version = `${new Date()
+                                                .toISOString()     // YYYY-MM-DDTHH:MM:SS...
+                                                .split('T')[0]     // gives YYYY-MM-DD
+                                                .substr(2)         // YYYY -> YY
+                                                .replace('-', '')  // YY-MM-DD -> YYMM-DD
+                                                .replace('-', '.') // YYMM-DD -> YYMM.DD
+                                        }.${process.env.BUILD_NUMBER === undefined ? 0 : process.env.BUILD_NUMBER}`;
+                jsonContent.browser_action.default_title = "Ultrawidify Testing";
 
-          return JSON.stringify(jsonContent, null, 2);
-        },
+                // because we don't want web-ext to submit this as proper release
+                delete jsonContent.applications;
+              }
+
+              if (process.env.BROWSER !== 'firefox') {
+                jsonContent.version = jsonContent.version.replace(/[a-zA-Z-]/g, '');
+                try {
+                  delete jsonContent.options_ui.browser_style;
+                } catch (e) { }
+                try {
+                  delete jsonContent.background.scripts;
+                } catch (e) {}
+              } else {
+                delete jsonContent.background.service_worker;
+              }
+
+              return JSON.stringify(jsonContent, null, 2);
+            },
+          },
+        ]
+      }
+    ),
+    new WebpackShellPluginNext({
+      onBuildEnd: {
+        scripts: ['node scripts/remove-evals.js'],
       },
-    ]),
-    new WebpackShellPlugin({
-      onBuildEnd: ['node scripts/remove-evals.js'],
     }),
     new webpack.DefinePlugin({
       'process.env.BROWSER': JSON.stringify(process.env.BROWSER),
@@ -235,9 +240,9 @@ if (config.mode === 'production') {
 }
 
 if (process.env.HMR === 'true') {
-  config.plugins = (config.plugins || []).concat([
-    new ChromeExtensionReloader(),
-  ]);
+  // config.plugins = (config.plugins || []).concat([
+  //   new ChromeExtensionReloader(),
+  // ]);
 }
 
 function transformHtml(content) {
